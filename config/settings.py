2c5b0a86dc4d99a815190fb7afe44f9a017b6a14
frublev <f.rublev@gmail.com>
2025-11-09 19:36:38 +0100

    create_superuser added

--- config/settings.py
+++ config/settings.py
@@ -99,15 +99,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST', 'localhost'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600)
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': os.getenv('DB_NAME'),
+    #         'USER': os.getenv('DB_USER'),
+    #         'PASSWORD': os.getenv('DB_PASSWORD'),
+    #         'HOST': os.getenv('DB_HOST', 'localhost'),
+    #         'PORT': os.getenv('DB_PORT', '5432'),
+    #     }
+    # }
 else:
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600)
