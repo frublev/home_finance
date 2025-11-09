8eabad53ef284b59e2c909b2e4d652cf83c156ab
frublev <f.rublev@gmail.com>
2025-11-09 13:23:25 +0100

    prepare deploy v2

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+
+DEBUG = False
+#DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
