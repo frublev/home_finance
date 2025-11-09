05315ebf03f355d44c0486c5ddf3182fed3fdf7b
frublev <f.rublev@gmail.com>
2025-11-09 14:47:34 +0100

    create_superuser added

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t", "yes")
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
